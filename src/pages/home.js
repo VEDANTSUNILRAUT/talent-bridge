c71cb643496936e02f5da17908e8b1fb54394b8f
VEDANTSUNILRAUT <vedantsunilraut@gmail.com>
2025-01-05 11:18:06 +0530

    changes in home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Hero from '../components/herosection/Hero';
 import Layout from '../components/Layout';
 import About from '../components/about/About';
 
 function Home() {
-  return (
-    <Layout>
-      <Hero />
-      <About />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Hero />
+            <About />
+        </Layout>
+    );
 }
 
 export default Home;
