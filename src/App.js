d6015ee1768b0ec8259abd2898b5bb59a1978c87
VEDANTSUNILRAUT <vedantsunilraut@gmail.com>
2025-01-08 18:04:08 +0530

    Changes in the Names of flies and folder

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Components
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 
 // Pages
-import Home from "./pages/home";
+import Home from "./Pages/home";
+import Resume from "./Pages/resume";
+import Job from "./Pages/job";
 
 // Student Login
 import Login from "./Authentication/Student/login";
@@ -15,8 +17,6 @@ import Signup from "./Authentication/Student/signup";
 // Admin Login
 import AdminLogin from "./Authentication/Admin/adminlogin";
 import AdminSignup from "./Authentication/Admin/adminsignup";
-// Resume
-import Resume from "./pages/resume";
 
 function App() {
   return (
@@ -29,6 +29,7 @@ function App() {
         <Route path="/adminlogin" element={<AdminLogin/>}/>
         <Route path="/adminsignup" element={<AdminSignup />} />
         <Route path="/resume" element={<Resume/>}/>
+        <Route path="/job" element={<Job />} />
       </Routes>
       <Footer />
     </>
