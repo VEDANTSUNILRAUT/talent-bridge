6ec75ba748399f3bb78679219990297e0cc22bf9
lokesh <21be0158@sipnaengg.ac.in>
2025-01-15 21:55:19 +0530

    ChanginHomePage

---
+++ frontend/src/Pages/aboutss.js
@@ -0,0 +1,10 @@
+import About from "../components/About/about";
+import Testimonial from "./testimonial";
+
+export default function Aboutss(){
+    return(
+        <>
+        <About/>
+        <Testimonial/></>
+    )
+}
