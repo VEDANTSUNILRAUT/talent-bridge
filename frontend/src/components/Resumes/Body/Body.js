e66dd11eff268147717b4cd9b1918edc46fc8521
lokesh <21be0158@sipnaengg.ac.in>
2025-04-08 23:58:58 +0530

    changing color

--- frontend/src/components/Resumes/Body/Body.js
+++ frontend/src/components/Resumes/Body/Body.js
@@ -9,7 +9,7 @@ import ClassicTemplate from "../Template/ClassicTemplate";
 import styles from "./Body.module.css";
 
 function Body() {
-  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+  const colors = ["#239ce2","#0bc5ea", "#a0aec0","#000000"];
   const sections = {
     basicInfo: "Basic Info",
     workExp: "Work Experience",
